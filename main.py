5e3636321bdfbf3b75f88b13cd3661c56e270786
Liza217-1678 <lizagorbacevic@gmail.com>
2023-12-08 14:28:17 +0300

    first commit

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-people = int(input('количество человек:'))
-c = int(input('число в считалке:'))
-print(' каждый выбывает', c, 'человек')
-a = [i for i in range(1, people+1)]
-start = 0
-while len (a)>1:
-    print('людей:', a)
-    print('начало счета', a[start])
-    delete = (start+c-1)%len(a)
-    if a[delete] == a[-1]:
-        start = 0
-    else: start = delete
-    print('номер человека', a.pop(delete))
-    print('остался', a[0])
+def highest(l):
+    Max = l[0]
+    for num in l:
+        if Max < num:
+            Max = num
+    return Max
 
 
-
+ highest ([77,48,19,17,93,90])
